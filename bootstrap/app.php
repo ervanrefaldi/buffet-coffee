1e3dab585124ebb096dc2689a249af36a4089a27
RenzIP <zenkun.enterkill13@gmail.com>
2026-01-23 19:19:08 +0700

    .

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies (required for Cloud Run)
+        $middleware->trustProxies(at: '*');
+        
         $middleware->validateCsrfTokens(except: [
             '/logout',
         ]);
